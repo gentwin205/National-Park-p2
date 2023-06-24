fdd3cec06e4b111e0ac1625c1795415125371eb3
Paul Ignagni <icerice965@gmail.com>
2023-06-24 18:36:53 -0400

    Push 4

--- src/main/java/com/techelevator/dao/JdbcSiteDao.java
+++ src/main/java/com/techelevator/dao/JdbcSiteDao.java
@@ -1,6 +1,8 @@
 package com.techelevator.dao;
 
+import com.techelevator.exception.DaoException;
 import com.techelevator.model.Site;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 
@@ -18,12 +20,35 @@ public class JdbcSiteDao implements SiteDao {
 
     @Override
     public List<Site> getSitesWithRVAccessByParkId(int parkId) {
-        return new ArrayList<>();
+        String sql = "SELECT * FROM site JOIN campground USING(campground_id) WHERE park_id = ? " +
+                "AND max_rv_length > 0;";
+        List <Site> sites = new ArrayList<>();
+        try {
+            SqlRowSet results = jdbcTemplate.queryForRowSet(sql, parkId);
+            while(results.next()) {
+                sites.add(mapRowToSite(results));
+            }
+        } catch (CannotGetJdbcConnectionException e) {
+            throw new DaoException("Cannot connect to database", e);
+        }
+        return sites;
     }
 
     @Override
     public List<Site> getSitesWithoutReservationByParkId(int parkId) {
-        return new ArrayList<>();
+        String sql = "SELECT * FROM site JOIN campground USING(campground_id) JOIN reservation USING (site_id) " +
+                "WHERE park_id = ? " +
+                "AND max_rv_length > 0 AND to_date < '2023-06-24';";
+        List <Site> sites = new ArrayList<>();
+        try {
+            SqlRowSet results = jdbcTemplate.queryForRowSet(sql, parkId);
+            while(results.next()) {
+                sites.add(mapRowToSite(results));
+            }
+        } catch (CannotGetJdbcConnectionException e) {
+            throw new DaoException("Cannot connect to database", e);
+        }
+        return sites;
     }
 
     private Site mapRowToSite(SqlRowSet results) {
--- src/test/java/com/techelevator/dao/JdbcSiteDaoTests.java
+++ src/test/java/com/techelevator/dao/JdbcSiteDaoTests.java
@@ -1,11 +1,13 @@
 package com.techelevator.dao;
 
+import com.techelevator.model.Reservation;
 import com.techelevator.model.Site;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -21,7 +23,13 @@ public class JdbcSiteDaoTests extends BaseDaoTests {
 
     @Test
     public void getSitesWithRVAccessByParkId_Should_Return_Sites_With_Positive_RV_Length() {
-        Assert.fail();
+        List<Site> sites = dao.getSitesWithRVAccessByParkId(1);
+        Assert.assertEquals(2, sites.size());
+        Site site1 = mapValuesToSite(1,1,1,10,true,33, true);
+        Site site2 = mapValuesToSite(2,1,2,10,true,30, true);
+        Assert.assertEquals(site1, sites.get(0));
+        Assert.assertEquals(site2, sites.get(1));
+
     }
 
     @Test
@@ -30,4 +38,26 @@ public class JdbcSiteDaoTests extends BaseDaoTests {
         assertEquals("Incorrect count of currently available sites", 5, sites.size());
 
     }
+
+    private Site mapValuesToSite(int site_id, int campground_id, int site_number, int max_occupancy, boolean accessible,
+                                 int max_rv_length, boolean utilities) {
+        Site site = new Site();
+        site.setSiteId(site_id);
+        site.setCampgroundId(campground_id);
+        site.setSiteNumber(site_number);
+        site.setMaxOccupancy(max_occupancy);
+        site.setAccessible(accessible);
+        site.setMaxRvLength(max_rv_length);
+        site.setUtilities(utilities);
+        return site;
+    }
+    private void assertSitesMatch (Site expected, Site actual){
+        Assert.assertEquals(expected.getSiteId(), actual.getSiteId());
+        Assert.assertEquals(expected.getCampgroundId(), actual.getCampgroundId());
+        Assert.assertEquals(expected.getSiteNumber(), actual.getSiteNumber());
+        Assert.assertEquals(expected.getMaxOccupancy(), actual.getMaxOccupancy());
+        Assert.assertEquals(expected.isAccessible(), actual.isAccessible());
+        Assert.assertEquals(expected.isUtilities(), actual.isUtilities());
+    }
+
 }
